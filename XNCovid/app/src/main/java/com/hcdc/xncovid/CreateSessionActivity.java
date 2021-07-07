d5a2072251237b0c0885f309de4d4e8949b70b16
hoai.ho <hoconghoai@gmail.com>
2021-07-07 23:55:08 +0700

    hoàn thiện CreateSessionActivity

--- XNCovid/app/src/main/java/com/hcdc/xncovid/CreateSessionActivity.java
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/CreateSessionActivity.java
@@ -1,10 +1,13 @@
 package com.hcdc.xncovid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CreateSessionActivity extends AppCompatActivity {
 
@@ -17,5 +20,9 @@ public class CreateSessionActivity extends AppCompatActivity {
                 R.array.province, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
