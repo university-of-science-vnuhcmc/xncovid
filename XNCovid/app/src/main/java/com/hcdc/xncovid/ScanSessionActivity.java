45017da1ae0f5b2916e4daf76de17cde1b0390b0
trantin1997 <trantin1997@gmail.com>
2021-07-07 17:51:16 +0700

    cm SessionInfo Activity

---
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/ScanSessionActivity.java
@@ -0,0 +1,138 @@
+package com.hcdc.xncovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ScanSessionActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler   {
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView mScannerView;
+    String sessionId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scansession);
+        int apiVersion = android.os.Build.VERSION.SDK_INT;
+        if (apiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (!checkPermission()) {
+                requestPermission();
+            }
+        }
+
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
+        mScannerView = new ZXingScannerView(this);
+        contentFrame.addView(mScannerView);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+
+    public void clickCancel(View view)
+    {
+
+        onBackPressed();
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        setContentView(R.layout.activity_main);
+        Pattern p = Pattern.compile("id=([A-z0-9-]*)");
+        String raw = result.getText();
+        Matcher m = p.matcher(raw);
+        m.find();
+        String id = m.group(1);
+        sessionId = id;
+        showResult();
+    }
+
+    private  void showResult(){
+        Intent intent = new Intent(getApplicationContext(), SessionInfoActivity.class);
+        intent.putExtra("xn_session", sessionId);
+        startActivity(intent);
+    }
+    private boolean checkPermission() {
+        return (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (grantResults.length > 0) {
+
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (cameraAccepted) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access camera", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access and camera", Toast.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                                showMessage("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                                            REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessage(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(ScanSessionActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+}
---
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/SessionInfoActivity.java
@@ -0,0 +1,33 @@
+package com.hcdc.xncovid;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.hcdc.xncovid.databinding.ActivitySessionInfoBinding;
+
+public class SessionInfoActivity extends AppCompatActivity {
+    String xn_session;
+    private TextView tenphien;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent=this.getIntent();
+        xn_session = getIntent().getExtras().getString("xn_session");
+        setContentView(R.layout.activity_session_info);
+        tenphien=(TextView) findViewById(R.id.xn_covid19_);
+        tenphien.setText(xn_session);
+    }
+
+}
