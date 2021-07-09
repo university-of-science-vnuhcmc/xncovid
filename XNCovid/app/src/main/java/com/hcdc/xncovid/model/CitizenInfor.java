0646dc3595ea425a92a68bfca5e1f3190d287697
trantin1997 <trantin1997@gmail.com>
2021-07-10 00:49:22 +0700

    cm code group test

---
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/model/CitizenInfor.java
@@ -0,0 +1,16 @@
+package com.hcdc.xncovid.model;
+
+public class CitizenInfor {
+    public String FullName ;
+    public String HandPhone;
+    public String CitizenIdentifierCode ;
+    public String YearOfBirth ;
+    public String Address;
+    public String ApartmentNo ;
+    public String StreetName ;
+    public String WardID ;
+    public String DistrictID ;
+    public String ProvinceID ;
+    public String QRCode ;
+    public int QRCodeType ;
+}
--- XNCovid/app/src/main/java/com/hcdc/xncovid/util/Caller.java
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/util/Caller.java
@@ -27,19 +27,21 @@ import java.util.Optional;
 
 public class Caller {
     public void call(Context context, String apiName, APIRequest req, Type type, ICallback callback, String url, int method){
+        HttpsTrustManager.allowAllSSL();
         RequestQueue queue = Volley.newRequestQueue(context);
         if(url == null || url.isEmpty()){
             url = "https://xncovid.uit.edu.vn:7070/api/";
         }
         url = url + apiName;
-        UserInfo userInfo = ((MyApplication)((Activity)(context)).getApplication()).getUserInfo();
-        if(userInfo != null){
-            req.Email = userInfo.Email;
-            req.Token = userInfo.Token;
-        }
+
         JSONObject jsonReq = null;
         try{
             if(req != null){
+                UserInfo userInfo = ((MyApplication)((Activity)(context)).getApplication()).getUserInfo();
+                if(userInfo != null){
+                    req.Email = userInfo.Email;
+                    req.Token = userInfo.Token;
+                }
                 jsonReq = new JSONObject(new Gson().toJson(req));
             }
         } catch (JSONException e)
@@ -80,7 +82,7 @@ public class Caller {
                         new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        callback.callback(new Gson().fromJson(response.toString(), type));
+                        callback.callback(response);
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -93,6 +95,7 @@ public class Caller {
                                 .show();
                     }
                 });
+                queue.add(jsonObjectRequest);
                 break;
         }
 
