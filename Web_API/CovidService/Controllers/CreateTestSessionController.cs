fbd363e8672928eb66d5b140ed052bb055883c0b
nhu.hoang <nhuhoang1095@gmail.com>
2021-07-10 15:03:27 +0700

    update

--- Web_API/CovidService/Controllers/CreateTestSessionController.cs
+++ Web_API/CovidService/Controllers/CreateTestSessionController.cs
@@ -1,8 +1,11 @@
 ﻿using CovidService.Models;
+using CovidService.Utility;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,6 +17,7 @@ namespace CovidService.Controllers
     {
         public CreateTestSessionResponse Post([FromBody]CreateTestSessionRequest objReq)
         {
+            
             CreateTestSessionResponse objRes = new CreateTestSessionResponse();
             try
             {
@@ -23,6 +27,7 @@ namespace CovidService.Controllers
                     objRes.returnMess = "Object request is null";
                     return objRes;
                 }
+                LogWriter.WriteLogMsg(JsonConvert.SerializeObject(objReq));
                 if (string.IsNullOrEmpty(objReq.SessionName))
                 {
                     objRes.returnCode = 1001;
@@ -35,30 +40,38 @@ namespace CovidService.Controllers
                     objRes.returnMess = "FullLocation is null or empty";
                     return objRes;
                 }
+                if (string.IsNullOrEmpty(objReq.TestingDate))
+                {
+                    objRes.returnCode = 1003;
+                    objRes.returnMess = "FullLocation is null or empty";
+                    return objRes;
+                }
 
                 string sqlString = SqlHelper.sqlString;
                 List<SqlParameter> parameters = new List<SqlParameter>();
                 SqlHelper.AddParameter(ref parameters, "@CovidTestingSessionName", System.Data.SqlDbType.NVarChar, 64, objReq.SessionName);
-                SqlHelper.AddParameter(ref parameters, "@Address", System.Data.SqlDbType.BigInt, objReq.FullLocation);
-                SqlHelper.AddParameter(ref parameters, "@ApartmentNo", System.Data.SqlDbType.VarChar, 64, objReq.ApartmentNo);
+                SqlHelper.AddParameter(ref parameters, "@Address", System.Data.SqlDbType.NVarChar, 256, objReq.FullLocation);
+                SqlHelper.AddParameter(ref parameters, "@ApartmentNo", System.Data.SqlDbType.NVarChar, 128, objReq.ApartmentNo);
+                SqlHelper.AddParameter(ref parameters, "@FromTestingDate", System.Data.SqlDbType.DateTime, DateTime.ParseExact(objReq.TestingDate, "yyyyMMddHHmm", CultureInfo.InvariantCulture));
+               // SqlHelper.AddParameter(ref parameters, "@FromTestingDate", System.Data.SqlDbType.DateTime, DateTime.Now);
                 SqlHelper.AddParameter(ref parameters, "@StreetName", System.Data.SqlDbType.NVarChar, 128, objReq.StreetName);
                 SqlHelper.AddParameter(ref parameters, "@WardID", System.Data.SqlDbType.BigInt, objReq.WardID);
                 SqlHelper.AddParameter(ref parameters, "@DistrictID", System.Data.SqlDbType.BigInt, objReq.DistrictID);
                 SqlHelper.AddParameter(ref parameters, "@ProvinceID", System.Data.SqlDbType.BigInt, objReq.ProvinceID);
+                //SqlHelper.AddParameter(ref parameters, "@ProvinceName", System.Data.SqlDbType.NVarChar, 128, "");
                 SqlHelper.AddParameter(ref parameters, "@Note", System.Data.SqlDbType.NVarChar, 1000, objReq.Note);
+                SqlHelper.AddParameter(ref parameters, "@CovidTestingSessionID", System.Data.SqlDbType.Int, ParameterDirection.Output);
                 SqlHelper.AddParameter(ref parameters, "@ReturnValue", System.Data.SqlDbType.Int, ParameterDirection.ReturnValue);
-                SqlHelper.ExecuteNonQuery(sqlString, CommandType.StoredProcedure, "dbo.uspAddCovidSpecimen", parameters.ToArray());
+                SqlHelper.ExecuteNonQuery(sqlString, CommandType.StoredProcedure, "dbo.uspAddCovidTestingSession", parameters.ToArray());
                 int intReturnValue = Convert.ToInt32(parameters[parameters.Count - 1].Value);
                 if (intReturnValue != 1)
                 {
-                    objRes.returnCode = 1002;
+                    objRes.returnCode = 1004;
                     objRes.returnMess = "DB return fail, ReturnCode: " + intReturnValue;
                     return objRes;
                 }
                 long loCovidSpecimenID = Convert.ToInt32(parameters[parameters.Count - 2].Value);
-                Random rd = new Random();
-                int i = rd.Next(7, 30);
-                objRes.SessionID = i;
+                objRes.SessionID = loCovidSpecimenID;
                 objRes.returnCode = 1;
                 objRes.returnMess = "Success";
                 return objRes;
@@ -66,7 +79,7 @@ namespace CovidService.Controllers
             catch (Exception ex)
             {
                 objRes.returnCode = -1;
-                objRes.returnMess = ex.ToString();
+                LogWriter.WriteException(ex);   
                 return objRes;
             }
         }
