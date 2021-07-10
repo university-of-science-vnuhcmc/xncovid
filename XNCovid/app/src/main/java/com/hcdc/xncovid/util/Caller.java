ce29cf46c87ddbe15382e00a3e66ac75e1a8cf86
hoai.ho <hoconghoai@gmail.com>
2021-07-10 10:27:13 +0700

    Revert "Revert "Merge branch 'mobile' of https://github.com/university-of-science-vnuhcmc/xncovid into mobile""
    This reverts commit 0837880ad70882bac3f1382257802f36d852d098.

--- XNCovid/app/src/main/java/com/hcdc/xncovid/util/Caller.java
+++ XNCovid/app/src/main/java/com/hcdc/xncovid/util/Caller.java
@@ -27,14 +27,10 @@ import java.util.Optional;
 
 public class Caller {
     public void call(Context context, String apiName, APIRequest req, Type type, ICallback callback, String url, int method){
+        HttpsTrustManager.allowAllSSL();
         RequestQueue queue = Volley.newRequestQueue(context);
         if(url == null || url.isEmpty()){
             url = "https://xncovid.uit.edu.vn:7070/api/";
-            UserInfo userInfo = ((MyApplication)((Activity)(context)).getApplication()).getUserInfo();
-            if(userInfo != null){
-                req.Email = userInfo.Email;
-                req.Token = userInfo.Token;
-            }
         }
         url = url + apiName;
         UserInfo userInfo = ((MyApplication)((Activity)(context)).getApplication()).getUserInfo();
@@ -45,6 +41,11 @@ public class Caller {
         JSONObject jsonReq = null;
         try{
             if(req != null){
+                UserInfo userInfo = ((MyApplication)((Activity)(context)).getApplication()).getUserInfo();
+                if(userInfo != null){
+                    req.Email = userInfo.Email;
+                    req.Token = userInfo.Token;
+                }
                 jsonReq = new JSONObject(new Gson().toJson(req));
             }
         } catch (JSONException e)
@@ -85,7 +86,7 @@ public class Caller {
                         new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        callback.callback(new Gson().fromJson(response.toString(), type));
+                        callback.callback(response);
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -98,6 +99,7 @@ public class Caller {
                                 .show();
                     }
                 });
+                queue.add(jsonObjectRequest);
                 break;
         }
 
